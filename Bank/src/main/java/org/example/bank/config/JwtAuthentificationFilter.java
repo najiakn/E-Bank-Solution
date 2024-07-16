34dbc02fe413b8d8ec0a563eb3a37d4394c1f9ba
najia <najiaknizi199@gmail.com>
2024-07-16 16:51:44 +0100

    add jjwt dependencies

--- Bank/src/main/java/org/example/bank/config/JwtAuthentificationFilter.java
+++ Bank/src/main/java/org/example/bank/config/JwtAuthentificationFilter.java
@@ -10,10 +10,23 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 public class JwtAuthentificationFilter extends OncePerRequestFilter {
+    private  final JwtService jwtService;
+
+
+
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
                                   @NonNull HttpServletResponse response,
                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String autheHeader=request.getHeader("Authorization");
+         final String jwt;
+         final String UserEmail;
+         if(autheHeader == null || !autheHeader.startsWith("Bearer ")){
+             filterChain.doFilter(request,response);
+             return;
+         }
+         jwt=autheHeader.substring(7);
+        UserEmail=jwtService.extracUsername(jwt);
 
     }
 }
---
+++ Bank/src/main/java/org/example/bank/config/JwtService.java
@@ -0,0 +1,10 @@
+package org.example.bank.config;
+
+import org.springframework.stereotype.Service;
+
+public class JwtService {
+    @Service
+    public String extracUsername(String token) {
+        return null;
+    }
+}
